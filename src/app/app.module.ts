d6933f8d897dffaa81df530729cc196112c2cf5c
Alex B <Blah_pb85@student.itstep.org>
2023-06-14 16:48:35 +0300

    #1 Сделана первичная верстка и структура проекта

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componenet/header/headerTop/header.component';
+import { HeaderBottomComponent } from './componenet/header/header-bottom/header-bottom.component';
+import { NavbarComponent } from './componenet/main/navbar/navbar.component';
+import { MainComponent } from './componenet/main/main/main.component';
+import { FooterComponent } from './componenet/footer/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderBottomComponent,
+    NavbarComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
